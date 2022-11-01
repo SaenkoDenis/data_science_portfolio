381f8b66c57484152eca94252b7743fd1c1785fe
Peresvet <t9221611197@gmail.com>
2022-11-01 12:39:32 +0500

    Незначительные изменения

--- leetcode/511. Game Play Analysis I.sql.py
+++ leetcode/511. Game Play Analysis I.sql.py
@@ -35,5 +35,10 @@ Result table:
 +-----------+-------------+
 """
 # Write your MySQL query statement below
-SELECT player_id, min(event_date) as first_login FROM Activity
-GROUP BY player_id;
+SELECT
+	player_id
+,	min(event_date) first_login
+FROM
+	activity
+GROUP BY
+	player_id;
